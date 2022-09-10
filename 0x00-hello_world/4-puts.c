41707b5d28d38d003d97955a563e0354f751c114
GriffGaff <bolajibaqi@guser.email>
2022-09-09 19:02:09 -0700

    4th task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Printing text
- * Return: Always 0 (Successful)
+ *
+ * Return: Always 0 (Correct)
  */
 int main (void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
