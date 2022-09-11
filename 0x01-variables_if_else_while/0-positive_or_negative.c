dcf3b7ddbc8cf8b0583c2fe471a7cecb0a4b1b8a
GriffGaff <bolajibaqi@guser.email>
2022-09-11 02:39:28 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,24 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /**
- * main - print if any number is positive, zero, or negative
- *
- * Description: using the main function
- * this prints "programming is positive, zero, or negative"
- * Return: 0
+ * main - definses the main purpose of the code
+ * Return: gives a response
  */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-	printf("%i is positive\n", n);
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf("%d is negative\n", n);
+	/* your code goes there */
+	return (0);
 }
-else if (n==0)
-{
-	printf("%i is zero\n", n);
-}
-else if (n < 0)
-{
-	printf("%i is negative\n", n);
-}
-return (0);
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
- * main - print if number is positive, zero or negative
- *
- * Description: using the man function 
- * this program prints "Pogram is positive, zero or negatve"
- * Return: 0
+ * main - definses the main purpose of the code
+ * Return: gives a response
  */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-int n:
-int l;
+	int n, m;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-
-if (l > 5)
-{
-	printf("last digit of %d is %d and is greater than 5\n", n, l);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
+	if (m == 0)
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	else if (m > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	/* your code goes there */
+	return (0);
 }
-else if (l == 0)
-{
-	printf("lastdgit of d% is d% and is 0\n", n, l);
-}
-else
-{
-	printf("last digit of d% is d% and is less than 6 and not 0\n", n, l);
-}
-return (0);
-
