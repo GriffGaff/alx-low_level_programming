4787d10e1f904bc7791d0066718a01d5b2e94d23
GriffGaff <bolajibaqi@guser.email>
2022-09-12 17:58:20 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Return: passes output
+ */
+int main(void)
+{
+	int n = '0';
+
+	for (n = '0'; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
