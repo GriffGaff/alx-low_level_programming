87d7d050089b8040ed6bbe6bc7853a863c853a55
GriffGaff <bolajibaqi@guser.email>
2022-09-12 17:41:08 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Return: passes output
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+		printf("n");
+	printf("\n");
+	return (0);
+}
