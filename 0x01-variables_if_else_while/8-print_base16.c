82b5b604f9dabcbb85dcbd134c774cbeb8db542f
GriffGaff <bolajibaqi@guser.email>
2022-09-12 18:26:45 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	int n = 0;
+	int n = '0';
 	char alpha = 'A';
 
-	for (n = 0; n < 10; n++)
+	for (n = '0'; n <= '9'; n++)
 		putchar(n);
 	for (alpha = 'A'; alpha < 'G'; alpha++)
 		putchar(alpha);
