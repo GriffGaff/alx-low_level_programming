043451579844362302226c4988c18720848db428
GriffGaff <bolajibaqi@guser.email>
2022-09-13 18:59:55 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - function
+ * @c: character to be checked
+ * Return: returns 1 if lowercase, returns 0 otherwise
+ */
+int _islower(int c)
+{
+		if (c == 'a' && c <= 'z')
+			return (1);
+		else
+			return (0);
+}
