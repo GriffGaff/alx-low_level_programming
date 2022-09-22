80d4b265d5f940ac5fc9ea75f355405bbd2087f1
GriffGaff <bolajibaqi@guser.email>
2022-09-22 06:58:31 -0700

    task 0

---
+++ 0x04-more_funtions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - function that checks for uppercase characters
+ *
+ * @c: characters to be cheecked
+ * Return: 1 , if character is upper.0 if lower
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' &&  c <= 'Z')
+		return (1);
+	else
+		return ('0');
+}
---
+++ 0x04-more_funtions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Working on more functions and more nested loops with C programming.
